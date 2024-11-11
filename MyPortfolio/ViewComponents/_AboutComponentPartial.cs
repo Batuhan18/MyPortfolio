3ccd17a85a9df56999edfa942923b2c59bbcd2f3
Batuhan18 <54512066+Batuhan18@users.noreply.github.com>
2024-11-12 01:18:01 +0300

    Add project files.

---
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _AboutComponentPartial:ViewComponent
+    {
+        MyPortfolioContext contex=new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.aboutTitle=contex.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.subDesc=contex.Abouts.Select(x=>x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail=contex.Abouts.Select(x=>x.Details).FirstOrDefault();
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio/ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _SkillComponentPartial : ViewComponent
+    {
+        MyPortfolioContext c = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = c.Skills.ToList();
+            return View(values);
+        }
+    }
+}
